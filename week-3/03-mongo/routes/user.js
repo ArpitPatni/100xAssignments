656fc47098d5c3ff3a9fdbc1ab1f3db525a21d84
ArpitPatni <arpitpatni00@gmail.com>
2024-09-18 22:14:27 +0530

    user.js route assignment part completed

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -26,26 +26,38 @@ router.get('/courses', async(req, res) => {
     })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async(req, res) => {
     // Implement course purchase logic
-    const courseId=req.params.courseId;
-    const username=req.headers.username;
 
-    User.updateOne({
-        username:username
-    },{
-        purchasedCourses:{
-            "$push":courseId
+    
+  const courseId = req.params.courseId;
+    const username = req.headers.username;
+
+    await User.updateOne({
+        username: username
+    }, {
+        "$push": {
+            purchasedCourses: courseId
         }
     })
-
     res.json({
-        msg:"Purchase complete"
+        message: "Purchase complete!"
     })
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const user=await User.findOne({
+        username:req.headers.username
+    })
+    const courses=await Course.find({
+        _id:{
+            "$in":user.purchasedCourses
+        }
+    })
+    res.json({
+        courses:courses
+    })
 });
 
 module.exports = router
